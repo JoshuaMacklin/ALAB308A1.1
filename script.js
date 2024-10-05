ee9504d1a19b61bde369d14d7fd84eb91093350a
JoshuaMacklin <Joshua.macklin@pm.me>
2024-10-05 08:05:14 -0700

    Part 2: Trampolines

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ try {
     console.log(counter);
 }
 
+// Part 2: Trampolines
+
+// const facto = (nestedArray) => {
+    
+//   }
+
+// const trampoline = (f, ...args) => {
+//     let result = f(...args);
+//     while (typeof result === "function") {
+//       result = result();
+//     }
+//     return result;
+//   }
+  
+
+// const nestedArray = [1, [2, [3, [4, 5]]], 6, [7, [8, 9]]];
+
+// console.log(trampoline(facto(nestedArray)))
+// // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// console.log(trampoline(facto(nestedArray)));
+// // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
